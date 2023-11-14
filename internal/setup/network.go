2214d8093019f75935156fc274549dac7e91d9b0
ogabriel <gabrieloliver8991@gmail.com>
2023-11-14 20:30:16 -0300

    feat: network

--- internal/setup/network.go
+++ internal/setup/network.go
@@ -1,6 +1,20 @@
 package setup
 
-var Network = Setup{
-    Name: "network",
-    Packages: []string{
-        
+import "github.com/ogabriel/ricer/internal"
+
+var Network = internal.Setup{
+	Name: "network",
+	Packages: []string{
+		"networkmanager",
+		"network-manager-applet",
+		"libappindicator-gtk3",
+	},
+	After: func() {
+		commands := []string{
+			"sudo systemctl enable NetworkManager",
+			"sudo systemctl start NetworkManager",
+		}
+
+		internal.RunCommands(commands)
+	},
+}
