f4f9f5f6ffad6dd98dae0f41fdebae8ed65e7bc3
ogabriel <gabrieloliver8991@gmail.com>
2023-11-15 09:01:03 -0300

    feat: add setups

---
+++ internal/setup/setups.go
@@ -0,0 +1,19 @@
+package setup
+
+import "github.com/ogabriel/ricer/internal"
+
+var Setups = []internal.Setup{
+	Pacman,
+	Yay,
+	Sddm,
+	Audio,
+	Bluetooth,
+	Network,
+	Screen,
+	Fonts,
+	Thunar,
+	Gnome,
+	Hyprland,
+	HyprlandGit,
+	Theme,
+}
