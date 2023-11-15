412a75fc39be9006f7e8fcff33b1484924af8259
ogabriel <gabrieloliver8991@gmail.com>
2023-11-14 21:45:31 -0300

    fix: no args

--- cmd/installProfile.go
+++ cmd/installProfile.go
@@ -4,6 +4,8 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/ogabriel/ricer/internal"
 	"github.com/ogabriel/ricer/internal/profile"
 	"github.com/spf13/cobra"
@@ -14,7 +16,9 @@ var installProfileCmd = &cobra.Command{
 	Short: "Install any profile",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		if args[0] == "hyprland" {
+		if len(args) == 0 {
+			fmt.Println("Please specify a profile to install")
+		} else if args[0] == "hyprland" {
 			internal.RunProfile(profile.Hyprland)
 		} else if args[0] == "hyprland-git" {
 			internal.RunProfile(profile.HyprlandGit)
