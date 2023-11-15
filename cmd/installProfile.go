6c3c4f32c586828a0e805c967b731b49bb6648b9
ogabriel <gabrieloliver8991@gmail.com>
2023-11-14 21:39:12 -0300

    feat: call install-profile

--- cmd/installProfile.go
+++ cmd/installProfile.go
@@ -4,6 +4,8 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"github.com/ogabriel/ricer/internal"
+	"github.com/ogabriel/ricer/internal/profile"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +14,11 @@ var installProfileCmd = &cobra.Command{
 	Short: "Install any profile",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		if args[0] == "hyprland" {
+			internal.RunProfile(profile.Hyprland)
+		} else if args[0] == "hyprland-git" {
+			internal.RunProfile(profile.HyprlandGit)
+		}
 	},
 }
 
