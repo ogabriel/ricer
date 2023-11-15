8a796a958b89cd3fde7dc8aa627688db73dd9d80
ogabriel <gabrieloliver8991@gmail.com>
2023-11-14 21:14:33 -0300

    feat: hyprland profile

--- internal/profiles/hyprland.go
+++ internal/profiles/hyprland.go
@@ -1,3 +1,36 @@
 package profiles
 
+import (
+	"github.com/ogabriel/ricer/internal"
+	"github.com/ogabriel/ricer/internal/setup"
+)
 
+var extraHyprland = internal.Setup{
+	Name: "extra hyprland",
+	Packages: []string{
+		"mako",
+		"alacritty",
+		"waybar",
+		"swaylock",
+		"swayidle",
+		"rofi-lbonn-wayland",
+	},
+}
+
+var Hyprland = internal.Profile{
+	Name: "hyprland",
+	Setups: []internal.Setup{
+		setup.Pacman,
+		setup.Yay,
+		setup.Sddm,
+		setup.Audio,
+		setup.Bluetooth,
+		setup.Network,
+		setup.Screen,
+		setup.Fonts,
+		setup.Thunar,
+		setup.Gnome,
+		setup.Hyprland,
+		extraHyprland,
+	},
+}
