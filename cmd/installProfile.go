59582cace696fc8e346a86aaa974becf7df2e80d
ogabriel <gabrieloliver8991@gmail.com>
2023-11-14 21:33:19 -0300

    chore: rename install-profile

--- cmd/installProfile.go
+++ cmd/installProfile.go
@@ -1,19 +1,20 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
 package cmd
 
 import (
 	"github.com/spf13/cobra"
 )
 
-var installCmd = &cobra.Command{
-	Use:   "install",
+var installProfileCmd = &cobra.Command{
+	Use:   "installProfile",
 	Short: "Install any profile",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(installCmd)
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(installProfileCmd)
 }
