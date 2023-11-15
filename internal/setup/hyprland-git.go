90a7676d7c7df7628b8c426034d2a1010979144f
ogabriel <gabrieloliver8991@gmail.com>
2023-11-14 21:08:43 -0300

    feat: hyprland-git

---
+++ internal/setup/hyprland-git.go
@@ -0,0 +1,17 @@
+package setup
+
+import "github.com/ogabriel/ricer/internal"
+
+var HyprlandGit = internal.Setup{
+	Name: "hyprland-git",
+	Before: func() {
+		internal.RunCommand("sudo pacman -R xdg-desktop-portal-gtk xdg-desktop-portal-wlr")
+	},
+	Packages: []string{
+		"hyprland-git",
+		"qt5-wayland",
+		"qt6-wayland",
+		"xdg-desktop-portal",
+		"xdg-desktop-portal-hyprland-git",
+	},
+}
