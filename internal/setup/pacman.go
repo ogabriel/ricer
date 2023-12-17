c3b249c9f21138206a4321f12c35b203ad943ea0
ogabriel <gabrieloliver8991@gmail.com>
2023-12-17 16:23:26 -0300

    update pacman

--- internal/setup/pacman.go
+++ internal/setup/pacman.go
@@ -6,8 +6,8 @@ var Pacman = internal.Setup{
 	Name: "pacman",
 	Before: func() {
 		commands := []string{
-			"sudo sed -i 's/#ILoveCandy/ILoveCandy/' /etc/pacman.conf",
 			"sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf",
+			"sudo sed -i 's/#Color/Color\nILoveCandy/' /etc/pacman.conf",
 		}
 
 		internal.RunCommands(commands)
